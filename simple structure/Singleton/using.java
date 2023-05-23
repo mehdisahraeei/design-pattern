c2583d37ad7273d4d937f11f3127fb91f4db736d
mahdisahraei <mahdi_sahrai@yahoo.com>
2023-05-23 19:10:20 +0330

    singleton added

---
+++ simple structure/Singleton/using.java
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+
+
+    public void ClickL(View view)
+    {
+        //------Singleton--used-----
+        Toast.makeText(context, Singleton.getInstance().setT("singletonActivity"), Toast.LENGTH_SHORT).show();
+    }
